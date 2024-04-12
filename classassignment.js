afd4bfccb891e2e7113e2785d01614c68881fc1b
papiyamohod <143395401+papiyamohod@users.noreply.github.com>
2024-04-12 20:46:25 +0530

    Add files via upload

---
+++ classassignment.js
@@ -0,0 +1,55 @@
+/*const Vote = (name, age) => {
+    if (age >= 18) {
+        console.log("Hey " + name + ", your age is " + age + ". You are eligible to vote.");
+    } else {
+        console.log("Hey " + name + ", your age is " + age + ". You are not eligible to vote.");
+    }
+};
+
+Vote("papiya", 20);
+Vote("priya", 9);*/
+/*2.
+const Square = (number) => {
+    const square1 = number * number;
+    console.log(`The square of ${number} is: ${square1}`);
+  };
+  const number=5;
+  Square(number);
+3.
+const divided = (a, b) => {
+    const np = Math.floor(a / b);
+    console.log(np);
+};
+divided(10, 3); 
+divided(15, 4); 
+4.
+const Digit = (number) => {
+    const Digit = Math.abs(number) % 10;
+    console.log(Digit);
+};
+Digit(321);
+Digit(999); 
+5.
+const Cube = number => Math.pow(number, 3);
+
+console.log(Cube(2)); 
+console.log(Cube(5));
+6.
+const Digit = number => {
+    const lastDigit = number % 10;
+    const firstDigit = parseInt(number.toString()[0]);
+    return firstDigit + lastDigit;
+};
+console.log(Digit(999));
+console.log(Digit(111)); 
+7.
+const Average = (salari) => {
+    let total = 0;
+    for (let i = 0; i < salari.length; i++) {
+        totalSalary += salari[i];
+    }
+    return total / salari.length;
+};
+const salari = [1100, 1200, 1300, 1400, 1500];
+console.log(Average(salari));*/
+
